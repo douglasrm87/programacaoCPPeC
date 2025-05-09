6574adcffad68ca8d018d949473cd87285f8f305
douglasrm87 <61594551+douglasrm87@users.noreply.github.com>
2025-05-09 23:48:25 +0000

    Ajuste para usar Debug

---
+++ Modulo06/exercicios/alomundo.c
@@ -0,0 +1,11 @@
+/*
+Faça um Programa que mostre a mensagem "Alo mundo" na tela.
+*/
+#include <stdio.h>
+#include <ctype.h>
+int main(){
+    printf("\nAlo mundo\n");
+    printf("\nAlo mundo\n");
+    printf("\nAlo mundo\n");
+    return 0;
+}
---
+++ Modulo06/formulabaskara.cpp
@@ -0,0 +1,20 @@
+/*Para liberar o VSCODE para acessar o GitHub na nuvem.
+Depois se logar normalmente.
+git config --global user.name "Douglas Mendes"
+git config --global user.email douglasrm87@hotmail.com
+*/
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main (int argc , char ** argv){
+    int a = 0;
+     int b = 0;
+     int c = 0;
+     float delta = 0.0; float x1 = 0.0; float x2 = 0.0;
+     // Escreva ()
+     cout << "Digite o Coeficiente a: " << endl;
+     // leia ()
+     cin >> a;
+     // Digitar o resto do programa aqui para baixo
+     return 0;
+}
